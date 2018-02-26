4f6e461711369eabf0008adb36d98dbb83fe89af
Ankush Shaw <shawank17198@gmail.com>
2018-02-26 17:28:55 +0530

    Doxygen config has been set-up.

---
+++ Source Code/main.cpp
@@ -0,0 +1,7 @@
+/*!
+* \author Ankush shaw 
+* \author Anirudh Kammampathy
+* \version 1.0
+* \date 26-02-2018 
+* \warning Implementation is yet to be taken up. Code structure might change here and there at completion. This is the proposed structure.
+*/
