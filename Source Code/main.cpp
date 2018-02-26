d93b465a30bbdcd30afcc79158afd1d70e4a6461
Anirudh8841 <36471208+Anirudh8841@users.noreply.github.com>
2018-02-26 17:38:03 +0530

    Update main.cpp

--- Source Code/main.cpp
+++ Source Code/main.cpp
@@ -1,6 +1,6 @@
 /*!
 * \author Ankush shaw 
-* \author Anirudh Kammampathy
+* \author Anirudh Khammampati
 * \version 1.0
 * \date 26-02-2018 
 * \warning Implementation is yet to be taken up. Code structure might change here and there at completion. This is the proposed structure.
